82173f402e290cec921494a4ed5eed0c22f3da02
Catalin Scurt <cata_scurt@yahoo.com>
2023-02-24 16:53:31 +0200

    initial commit

---
+++ src/app/CONFIG/firebase.config.ts
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyAq9NpVUtZi-JF8GiVvvYJernBtFYDtkJM",
+    authDomain: "myclone-21503.firebaseapp.com",
+    projectId: "myclone-21503",
+    storageBucket: "myclone-21503.appspot.com",
+    messagingSenderId: "541400137768",
+    appId: "1:541400137768:web:34eb4a5017dce4fa1cf51d",
+    measurementId: "G-B11D3R2HSV"
+  };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'myclone-21503',
+    appId: '1:541400137768:web:34eb4a5017dce4fa1cf51d',
+    storageBucket: 'myclone-21503.appspot.com',
+    apiKey: 'AIzaSyAq9NpVUtZi-JF8GiVvvYJernBtFYDtkJM',
+    authDomain: 'myclone-21503.firebaseapp.com',
+    messagingSenderId: '541400137768',
+    measurementId: 'G-B11D3R2HSV',
+  },
   production: false
 };
 
